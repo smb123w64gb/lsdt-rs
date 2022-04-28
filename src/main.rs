285c4158201d19453beb5a0caf0a96a2add027d4
smb123w64gb <smb123w64gb@gmail.com>
2022-04-28 08:46:15 -0700

    Spitting out packed assets

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 
+use std::*;
 use std::path::PathBuf;
 use std::io::BufReader;
 use std::fs::File;
+use std::path::Path;
+use std::fs::*;
 use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 use std::io::prelude::*;
 use flate2::read::ZlibDecoder;
@@ -27,7 +30,7 @@ fn extract(_ls_file: PathBuf, _dt_file: PathBuf,_out_folder: PathBuf) {
 
     //let d = File::open(_dt_file).unwrap();
     //print!("RF Compressed is {0} Bytes\nRF Offset is {1}\n",rf_file_info.size,rf_file_info.offset);
-    let  mut dt = BufReader::new(File::open(_dt_file).unwrap());
+    let mut dt = BufReader::new(File::open(_dt_file).unwrap());
     //Load our Data file into memory
     dt.seek(SeekFrom::Start(rf_file_info.offset as u64)).unwrap();
     //Seek to our RF file
@@ -35,12 +38,12 @@ fn extract(_ls_file: PathBuf, _dt_file: PathBuf,_out_folder: PathBuf) {
     //Alocate memory for rf
     dt.read_exact(&mut rf_data).unwrap();
     let mut filetest = File::create("testOut.rf").unwrap();
-    filetest.write_all(&rf_data);
+    filetest.write_all(&rf_data).unwrap();
     //Read from buffer into that alocated memory
     let mut rf_cursor = Cursor::new(&rf_data);
     let rf = rf::RFFile::read(&mut rf_cursor);
     filetest.seek(SeekFrom::Start(0x80)).unwrap();
-    filetest.write_all(&rf.debug_extract);
+    filetest.write_all(&rf.debug_extract).unwrap();
     let mut stringbuild: Vec<String> = Vec::new();
     let mut string_outs: Vec<String> = Vec::new();
     for n in &rf.entrys {
@@ -51,13 +54,33 @@ fn extract(_ls_file: PathBuf, _dt_file: PathBuf,_out_folder: PathBuf) {
         string_outs.push(stringbuild.join(""))
     
     }
-    let mut lsoffset: Vec<LSEntry> = Vec::new();
+    let mut lsoffset: LSEntry= LSEntry{
+        crc : 0,
+        offset : 0,
+        size : 0,
+        dt_index : None,
+        padding : None
+    };
     for n in 0..rf.entrys.len(){
+        if rf.entrys[n].is_folder{
+            if Path::new(&format!("{0}\\{1}",_out_folder.to_str().unwrap(),string_outs[n])).exists() == false
+            {
+            create_dir(format!("{0}\\{1}",_out_folder.to_str().unwrap(),string_outs[n]));
+            }
+        }
         if rf.entrys[n].is_compressed{
-        lsoffset.push(ls.find(&format!("data/{0}{1}",string_outs[n],if rf.entrys[n].is_compressed && rf.entrys[n].is_folder{
-            "packed"
-        }else{
-            ""
-        })));
-    }}
+            lsoffset = ls.find(&format!("data/{0}{1}",string_outs[n],if rf.entrys[n].is_compressed && rf.entrys[n].is_folder{"packed"}else{""}));
+            let mut cur_data = vec![0u8;lsoffset.size as usize];
+            dt.seek(SeekFrom::Start(lsoffset.offset as u64)).unwrap();
+            dt.read_exact(&mut cur_data).unwrap();
+            let mut fs_cursor = Cursor::new(&cur_data);
+            //let mut fs_decomp = Vec::new();
+            //let mut decomp_zlib = ZlibDecoder::new(fs_cursor);
+            //decomp_zlib.read_to_end(&mut fs_decomp).unwrap();
+            println!("{0}\\{1}",_out_folder.to_str().unwrap(),format!("{0}{1}",string_outs[n],if rf.entrys[n].is_compressed && rf.entrys[n].is_folder{"packed"}else{""}).replace("/","\\"));
+            std::fs::write(format!("{0}\\{1}",_out_folder.to_str().unwrap(),format!("{0}{1}",string_outs[n],if rf.entrys[n].is_compressed && rf.entrys[n].is_folder{"packed"}else{""}).replace("/","\\")),cur_data).unwrap();
+
+    }
+}
+
 }
